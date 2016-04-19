f00facc12c9a5268b6f202ea27a776dfe66220a8
davejohnston <dave.johnston@icloud.com>
2016-04-19 14:38:21 +0100

    Initial commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,17 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+#Rails.application.config.assets.precompile += %w( welcome.css )
+#Rails.application.config.assets.precompile += %w( welcome.js )
+
+
+Rails.application.config.assets.precompile += [ 'welcome.css', 'appviews.css', 'cssanimations.css', 'dashboards.css', 'forms.css', 'gallery.css', 'sur.css', 'mailbox.css', 'miscellaneous.css', 'pages.css', 'tables.css', 'uielements.css', 'widgets.css', 'commerce.css' ]
+Rails.application.config.assets.precompile += [ 'welcome.js', 'appviews.js', 'cssanimations.js', 'dashboards.js', 'forms.js', 'gallery.js', 'sur.js', 'mailbox.js', 'miscellaneous.js', 'pages.js', 'tables.js', 'uielements.js', 'widgets.js', 'commerce.js', 'vnf.js' ]
